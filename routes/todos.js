09f0c0efa366d20fe719d720bfd1938701caf3cb
YouMinTW <m791213p@gmail.com>
2020-09-02 13:33:55 +0800

    get/post a todo

--- routes/todos.js
+++ routes/todos.js
@@ -11,5 +11,29 @@ let todos = [
 router.get('/', function (req, res, next) {
   res.send(todos)
 })
+/* GET a specific todo. */
+router.get('/:todoId', function (req, res, next) {
+  const todoId = req.params.todoId
+  const todo = todos.find((todo) => todoId === String(todo.id))
+  if (!todo) {
+    res.status(404).send('The todo with the given ID was not found')
+    return
+  }
+  res.send(todo)
+})
+/* POST a specific todo. */
+router.post('/', function (req, res, next) {
+  if (!req.body.name || req.body.name.length < 3) {
+    res.status(400).send('Name is required and should be minimun 3 characters')
+    return
+  }
+  let todo = {
+    id: todos.length + 1, // 沒有資料庫，先手動新增ID
+    name: req.body.name, // 取得傳送來的name
+    done: false // 預設未完成
+  }
+  todos.push(todo)
+  res.send(todos)
+})
 
 module.exports = router
